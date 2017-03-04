e0c60a4d709a1fe0380f0f8a484896bc1555ea7a
Abdulazeez naji <abdulazeeznaji@yahoo.com>
2017-03-04 17:49:39 +0200

    done

---
+++ src/pages/home/home.ts
@@ -0,0 +1,37 @@
+import { Component , ViewChild } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation} from 'ionic-native'
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+    @ViewChild('map') mapElement;
+    map: any;
+  constructor(public navCtrl: NavController) {
+  }
+  ionViewDidLoad(){
+    this.loadMap()
+  }
+ loadMap(){
+ 
+    Geolocation.getCurrentPosition().then((position) => {
+ 
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+    }, (err) => {
+      console.log(err);
+    });
+ 
+  }
+ 
+}
